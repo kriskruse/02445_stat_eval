f7c33f9de9bcbfdc35e4bc7623ceb67e3fc5f0d8
Kris Kruse <kriskruse@hotmail.com>
2022-01-10 14:15:57 +0100

    good practise added to py file

--- Data_managment.py
+++ Data_managment.py
@@ -4,12 +4,20 @@ import numpy as np
 import pyreadr as pr
 
 
-robjects.r['load']("armdata.RData")
-matrix = robjects.r["armdata"]
+def main():
+    # Load the data form the rdata file into python, and bind it to a variable
+    robjects.r['load']("armdata.RData")
+    matrix = robjects.r["armdata"]
 
-armdata = np.array(matrix)
+    # convert the data to a numpy array, so we can use the numpy functionality
+    armdata = np.array(matrix)
 
-t = np.argwhere(np.isnan(armdata))
+    # Locating the indexes of the na data points. index read as (experiment, person, repetition, row, col)
+    # Keep in mind that in python we use, 0 indexing which is not the case when viewing data in R
+    t = np.argwhere(np.isnan(armdata))
+    print(t)
 
-print(t)
 
+# For good python style
+if __name__ == "__main__":
+    main()
