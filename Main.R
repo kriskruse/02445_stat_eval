32b41de1b7a32527caa92a2c3e1317388b2da1c8
Kris Kruse <kriskruse@hotmail.com>
2022-01-12 11:35:09 +0100

    removed redundant code

--- Main.R
+++ Main.R
@@ -4,7 +4,7 @@
 
 
 ############### data load
-load(file = "armdata.RData")
+load(file = "fixedarmdata.RData")
 ## access with armdata
 
 # Unpack the Experiments into their own lists
@@ -22,30 +22,7 @@ for (item in armdata){
 # for these access them with "eks1$per1[1]" for rep 1
 
 
-
 #### Fancy shit under here
 
-for (expnr in 1:16) { 
-  for (persnr in 1:10) {
-    for (repnr in 1:10) {
-      for (x in 1:100) {
-        for (y in 1:3) {
-          mat <- armdata[[expnr]][[persnr]][[repnr]]
-          if (is.na(mat[x, y])) {
-            li = c()
-            for (g in 1:10){
-              li = c(li,armdata[[expnr]][[persnr]][[g]][x,y])
-            }
-            if (expnr == 1) {
-              armdata[[expnr]][[persnr]][[repnr]][x, y] <-  mean(li, na.rm = TRUE)
-            } else {
-              armdata[[expnr]][[persnr]][[repnr]][x, y] <-  mean(li, na.rm = TRUE)
-            }
-          }
-        }
-      }
-    }
-  }
-}
-save(armdata, file = "fixedarmdata.RData")              
+   
                    
