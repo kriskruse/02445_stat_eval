1d32c8b30d69acadf71772cf97816fa74ac57104
Kris Kruse <kriskruse@hotmail.com>
2022-01-14 14:12:15 +0100

    some easier classes testing

--- Multiclass classification.py
+++ Multiclass classification.py
@@ -1,6 +1,5 @@
 import itertools as it
 import numpy as np
-import torch
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
@@ -8,8 +7,6 @@ from sklearn.model_selection import train_test_split
 
 seed = 42069
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print("Device : {}".format(device))
 
 df = pd.read_pickle("DataFrame.pkl")
 
@@ -43,20 +40,57 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.8, random
 
 # note: 0.01 seems to be the best C value, of the tested
 test_lst = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
-#lam = 0.01
+lam = 0.01
+# for lam in test_lst:
+
+lr1 = LogisticRegression(multi_class="ovr", C=lam, max_iter=100000)
+model = lr1.fit(X_train, Y_train)
+y_pred = model.predict(X_test)
+
+print("")
+print(f"lambda value: {lam}")
+print(f"train score {model.score(X_train, Y_train)}")
+print(f"test score {model.score(X_test, Y_test)}")
+
+lr2 = LogisticRegression(multi_class="multinomial", C=lam, max_iter=100000)
+model2 = lr2.fit(X_train, Y_train)
+y_pred2 = model2.predict(X_test)
+
+print("")
+print(f"lambda value: {lam}")
+print(f"train score {model2.score(X_train, Y_train)}")
+print(f"test score {model2.score(X_test, Y_test)}")
+
+# We now test with less labels
+# moving the classes values from experiment number, to distance classes
+# so we now have a class for each of the obstacles 6 distances
+# 0 no obstacle, 1 = 15cm, 2 = 22.5cm, 3 = 30cm, 4 = 37.5cm, 5 = 45cm
+# this is a bit hacky but we know the data so it should be ok
+clas_lst = [1, 2, 3, 4, 5, 0]
+Y = []
+for item in clas_lst:
+    if item == 0:
+        for i in range(100):
+            Y.append(item)
+    else:
+        for i in range(300):
+            Y.append(item)
+Y = np.array(Y)
+# print(np.size(Y))
+# print(Y)
+
+# we do the split again to get the right sizes for the new Y_train and test
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.8, random_state=1)
+
+print("")
+print("new sizes for simpler classification task")
+print(np.size(Y_train))
+print(np.size(Y_test))
+
 for lam in test_lst:
-    lr1 = LogisticRegression(multi_class="ovr", C=lam, max_iter=100000)
-    model = lr1.fit(X_train, Y_train)
-    y_pred = model.predict(X_test)
-
-    print("")
-    print(f"lambda value: {lam}")
-    print(f"train score {model.score(X_train, Y_train)}")
-    print(f"test score {model.score(X_test, Y_test)}")
-
-    lr2 = LogisticRegression(multi_class="multinomial", C=lam, max_iter=100000)
-    model2 = lr2.fit(X_train, Y_train)
-    y_pred2 = model2.predict(X_test)
+    lr3 = LogisticRegression(multi_class="multinomial", C=lam, max_iter=1000)
+    model3 = lr3.fit(X_train, Y_train)
+    y_pred3 = model2.predict(X_test)
 
     print("")
     print(f"lambda value: {lam}")
